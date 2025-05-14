bed29d049885f850c1e0c0debc28cadf2cd7a24f
Odai Mohammed Alkhalaf Alissa <112479069+odaia2@users.noreply.github.com>
2025-05-14 14:31:33 +0200

    update

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -2,11 +2,10 @@ import React from "react";
 import { Link } from "react-router-dom";
 import LanguageSwitcher from "./LanguageSwitcher";
 
-
 const Nav = () => {
   return (
     <nav className="navbar">
-      <div className="logo">Logo</div>
+      <Link to="/" className="logo">Logo</Link>
       <div className="nav-links">
         <Link to="/">Hjem</Link>
         <Link to="/kontakt">Kontakt oss</Link>
